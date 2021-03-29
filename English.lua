d67396e43a05e302e549ce9b555fcd3a6ce9a3ff
datoyacx <datoyacx@outlook.jp>
2021-03-29 18:27:46 +0700

    Added English Language

---
+++ English.lua
@@ -0,0 +1,75 @@
+return {
+    ["SelectLanguage"] = "Pilih Bahasa",
+    ["LoadingAsset"] = "Loading Asset",
+    ["LoadingData"] = "Loading Data",
+    ["LoadingComplete"] = "Loading Complete",
+    ["Loading"] = "Loading",
+
+    ["Weapon"] = "Weapon",
+    ["Boombox"] = "Boombox",
+    ["Pet"] = "Pet",
+    ["Inventory"] = "Inventory",
+    ["Shop"] = "Shop",
+    ["Game"] = "Game",
+    ["Achievements"] = "Achievements",
+    ["GhostBook"] = "GhostBook",
+    ["Hat"] = "Hat",
+
+    ["Damage"] = "Damage",
+    ["Rarity"] = "Rarity",
+
+    ["Equip"] = "Equip",
+    ["Equipped"] = "Equipped",
+
+    ["Kill"] = "Kill",
+    ["Headshot"] = "Headshot",
+    ["Assist"] = "Assist",
+    ["Leaderboard"] = "Leaderboard",
+    ["Name"] = "Name",
+
+    ["RevivePossessed"] = "PRESS <b>[ F ]</b> TO REVIVE",
+    ["PlayerPossessed"] = "%s Has Been Possessed",
+
+    ["KillEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> Kill %s",
+    ["AssistEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> [Assist] Kill %s",
+    ["HeadshotEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> <font color='rgb(255,0,0)'>[Headshot]</font> Kill %s",
+    ["SurviveRound"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> Survive Round",
+    ["SurviveRoundBoss"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font>You Guys Success Kill The Boss!",
+
+    ["NoPlayerIntermission"] = "There no player, do intermission again..",
+    ["LoadingMap"] = "Loading map",
+    ["Gameover"] = "Gameover",
+    ["EndWave"] = "Wave %s over",
+    ["StartWave"] = "Starting Wave %s",
+    ["TeleportingPlayer"] = "Teleporting player",
+    ["Intermission"] = "Intermission %s",
+    ["Gameplay"] = "Wave %s | %s Ghost(s) Left | Time Left %s",
+    ["GameplayBoss"] = "Wave Boss | Kill The Boss | Time Left %s",
+
+    ["Attack"] = "Attack",
+    ["Shot"] = "Shot",
+    ["Menu"] = "Menu",
+    ["Dash"] = "Dash",
+    ["Revive"] = "Revive",
+
+    ["NoPlayerSpectate"] = "There no player can you spectate",
+    ["SpectateNote"] = "You are spectate right now, to quit press [ T ]",
+    ["Spectate"] = "Spectate",
+
+    ["GameTutorial"] = "Game Tutorial",
+    ["GameTutorialNote"] = "How to play? just kill the ghost with weapon, by pressing [ 1 ] [ 2 ] to equip weapon or on mobile click box button in the middle bottom, this game basically like another zombie wave game in roblox",
+    ["Control"] = "Controls",
+    ["ControlNote"] = "Inventory - [ I ]<br />Spectate - [ T ]<br />Leaderboard - [ Tab ]<br />Dash - [ Space ]<br />Change Weapon - [ 1 ] [ 2 ]",
+    ["Close"] = "Close",
+    ["GameTutorialClose"] = "Press [ Space ] or Click This to Close",
+
+    ["FeedbackInteract"] = "PRESS <b>[ F ]</b> TO OPEN FEEDBACK CENTER",
+    ["FeedbackTitle"] = "Feedback Center",
+    ["FeedbackSend"] = "Send To Developer",
+    ["FeedbackSendLater"] = "Send feedback later",
+    ["FeedbackCooldown"] = "Cooldown (%s)",
+    ["FeedbackNotValid"] = "Feedback must be more than 5 character",
+    ["FeedbackSended"] = "Feedback send to Developer",
+    ["FeedbackMessage"] = "Write your feedback here",
+    ["Open"] = "Open"
+}
