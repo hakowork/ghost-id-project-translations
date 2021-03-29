f6ab03e8bf985e066de7c8cc3002c07e840b9782
datoyacx <datoyacx@outlook.jp>
2021-03-29 18:27:11 +0700

    Added Indonesia Language

---
+++ Indonesia.lua
@@ -0,0 +1,75 @@
+return {
+    ["SelectLanguage"] = "Select Language",
+    ["LoadingAsset"] = "Memuat Asset",
+    ["LoadingData"] = "Memuat Data",
+    ["LoadingComplete"] = "Selesai Memuat",
+    ["Loading"] = "Memuat",
+
+    ["Weapon"] = "Senjata",
+    ["Boombox"] = "Boombox",
+    ["Pet"] = "Pet",
+    ["Inventory"] = "Inventory",
+    ["Shop"] = "Toko",
+    ["Game"] = "Game",
+    ["Achievements"] = "Achievements",
+    ["GhostBook"] = "Buku Hantu",
+    ["Hat"] = "Topi",
+
+    ["Damage"] = "Kekuatan",
+    ["Rarity"] = "Kelangkaan",
+
+    ["Equip"] = "Pasang",
+    ["Equipped"] = "Terpasang",
+
+    ["Kill"] = "Membunuh",
+    ["Headshot"] = "Headshot",
+    ["Assist"] = "Membantu",
+    ["Leaderboard"] = "Papan Pemimpin",
+    ["Name"] = "Nama",
+
+    ["RevivePossessed"] = "TEKAN <b>[ F ]</b> UNTUK MENYELAMATKAN",
+    ["PlayerPossessed"] = "%s Telah Kesurupan",
+
+    ["KillEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> Membunuh %s",
+    ["AssistEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> [Bantu] Membunuh %s",
+    ["HeadshotEnemy"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> <font color='rgb(255,0,0)'>[Headshot]</font> Membunuh %s",
+    ["SurviveRound"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> Bertahan Hidup",
+    ["SurviveRoundBoss"] = "<font color='rgb(85,170,127)'>[%s]</font> <font color='rgb(255,255,127)'>[%s]</font> Kalian Sukses Bunuh Bos!",
+
+    ["NoPlayerIntermission"] = "Tidak ada pemain, Melakukan istirahat lagi .. ",
+    ["LoadingMap"] = "Memuat peta",
+    ["Gameover"] = "Permainan berakhir",
+    ["EndWave"] = "Gelombang %s berakhir",
+    ["StartWave"] = "Memulai gelombang %s",
+    ["TeleportingPlayer"] = "Pemain diteleportasi",
+    ["Intermission"] = "Istirahat  %s",
+    ["Gameplay"] = "Gelombang %s | %s Hantu Tersisa | Waktu Tersisa %s",
+    ["GameplayBoss"] = "Gelombang Bos | Bunuh Bos | Waktu Tersisa %s",
+
+    ["Attack"] = "Serang",
+    ["Shot"] = "Tembak",
+    ["Menu"] = "Menu",
+    ["Dash"] = "Dash",
+    ["Revive"] = "Revive",
+
+    ["NoPlayerSpectate"] = "Tidak ada pemain yang bisa Anda tonton",
+    ["SpectateNote"] = "Anda sedang menononton sekarang, untuk keluar tekan [ T ]",
+    ["Spectate"] = "Nonton",
+
+    ["GameTutorial"] = "Tutorial Permainan",
+    ["GameTutorialNote"] = "Bagaimana cara bermain? ya bunuh hantunya pake senjata, tekan [ 1 ] [ 2 ] untuk pake senjata atau jika di HP/Tablet klik tombol kotak di tenggah bawah, game ini pada dasarnya kek game zombie di roblox",
+    ["Control"] = "Controls",
+    ["ControlNote"] = "Inventory - [ I ]<br />Nonton - [ T ]<br />Papan Pemimpin - [ Tab ]<br />Dash - [ Space ]<br />Ganti Senjata - [ 1 ] [ 2 ]",
+    ["Close"] = "Tutup",
+    ["GameTutorialClose"] = "Tekan [ Space ] atau Klik Ini untuk Menutup",
+
+    ["FeedbackInteract"] = "TEKAN <b>[ F ]</b> UNTUK MEMBUKA PUSAT MASUKAN",
+    ["FeedbackTitle"] = "Pusat Masukan",
+    ["FeedbackSend"] = "Kirim Ke Developer",
+    ["FeedbackSendLater"] = "Kirim masukan nanti",
+    ["FeedbackCooldown"] = "Cooldown (%s)",
+    ["FeedbackNotValid"] = "Masukan harus lebih dari 5 kata",
+    ["FeedbackSended"] = "Masukan terkirim ke Developer",
+    ["FeedbackMessage"] = "Tulis masukanmu disini",
+    ["Open"] = "Buka"
+}
